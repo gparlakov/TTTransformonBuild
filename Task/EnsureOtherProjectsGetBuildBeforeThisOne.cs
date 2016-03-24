72c35c300359ac3dc569bf72474a0743e109ec82
Georgi Parlakov <gparlakov@abv.bg>
2016-03-24 18:44:58 +0200

    Add sample solution with example how to

---
+++ Task/EnsureOtherProjectsGetBuildBeforeThisOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class EnsureOtherProjectsGetBuildBeforeThisOne
+    {
+        public static void Main()
+        {
+            var te1 = Nom1.EnumFu.te1;
+
+            var mu1 = Nomenclature.EnumMu.Mu1;
+
+            Console.WriteLine(te1);
+            Console.WriteLine(mu1);
+        }
+    }
+}
---
+++ Task/TextTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DAIS.eBank.Trader.Services
+{    
+    public enum DAISComansNomenclature
+    {
+ 
+       Mu1 = 1,
+       Mu2 = 2,
+
+ 
+       te1 = 3,
+       te2 = 4,
+
+    }
+}
